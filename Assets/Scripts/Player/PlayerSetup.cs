50ca04d6e0334ed8e78d01ae12a0e99d894a06fe
packerbacker8 <chandler@gbpackerfans.com>
2018-08-11 01:12:35 -0600

    Starting to add syncing of movement across the network

---
+++ Assets/Scripts/Player/PlayerSetup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera worldCamera;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach(Behaviour component in componentsToDisable)
+            {
+                component.enabled = false;
+            }
+        }
+        else
+        {
+            worldCamera = Camera.main;
+            if(worldCamera != null)
+            {
+                //temporary - will be moved to a different location at a future date
+                Camera.main.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(worldCamera != null)
+        {
+            //temporary - will be moved to a different location at a future date
+            Camera.main.gameObject.SetActive(true);
+        }
+    }
+}
