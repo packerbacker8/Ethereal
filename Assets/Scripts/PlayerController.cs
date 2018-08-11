959d38d1e2808226906b41aeeadbbed204340e22
packerbacker8 <chandler@gbpackerfans.com>
2018-08-11 00:10:26 -0600

    Initial commit. Added gitignore. Added player movement basics.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public Camera playerCam;
+
+    public bool invertLook = false;
+    #region movement keycodes
+    [Header("Movement Keys")]
+    public KeyCode moveForward = KeyCode.W;
+    public KeyCode moveBackward = KeyCode.S;
+    public KeyCode strafeLeft = KeyCode.A;
+    public KeyCode strafeRight = KeyCode.D;
+    public KeyCode jump = KeyCode.Space;
+    public KeyCode walk = KeyCode.LeftShift;
+    public KeyCode crouch = KeyCode.LeftControl;
+    #endregion
+
+    #region combat keycodes
+    [Header("Combat Keys")]
+    public KeyCode primaryAction = KeyCode.Mouse0;
+    public KeyCode secondaryAction = KeyCode.Mouse1;
+    public KeyCode interact = KeyCode.E;
+    public KeyCode reload = KeyCode.R;
+    public KeyCode melee = KeyCode.F;
+    public KeyCode quickSwitch = KeyCode.Q;
+    public KeyCode quickGrenade = KeyCode.G;
+    #endregion
+
+    [Header("Movement")]
+    [SerializeField]
+    private float normalSpeed = 5f;
+    [SerializeField]
+    private float walkSpeedMultiplier = 0.5f;
+    private float walkSpeed;
+    private float currentSpeed;
+    [SerializeField]
+    private float mouseSensitivity = 3f;
+
+    private int invertedLook = -1;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        walkSpeed = normalSpeed * walkSpeedMultiplier;
+        currentSpeed = normalSpeed;
+        motor = this.GetComponent<PlayerMotor>();
+        if (playerCam != null)
+        {
+            motor.SetCamera(playerCam);
+        }
+        invertedLook = invertLook ? 1 : -1;
+    }
+
+    private void Update()
+    {
+        //calculate movement velocity as 3d vector
+        bool forwardMove = Input.GetKey(moveForward);
+        bool backwardMove = Input.GetKey(moveBackward);
+        bool leftMove = Input.GetKey(strafeLeft);
+        bool rightMove = Input.GetKey(strafeRight);
+        float xMovement = (leftMove ? -1 : 0) + (rightMove ? 1 : 0);
+        float zMovement = (backwardMove ? -1 : 0) + (forwardMove ? 1 : 0);
+        float yRotation = Input.GetAxisRaw("Mouse X");
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+
+        HandleWalking();
+
+
+        Vector3 moveXHorizontal = this.transform.right * xMovement;
+        Vector3 moveZHorizontal = this.transform.forward * zMovement;
+
+        Vector3 velocity = (moveXHorizontal + moveZHorizontal).normalized * currentSpeed;
+
+        motor.Move(velocity);
+
+        Vector3 rotation = new Vector3(0, yRotation, 0) * mouseSensitivity;
+
+        motor.RotateY(rotation);
+
+        Vector3 camRotation = new Vector3(xRotation * invertedLook, 0, 0) * mouseSensitivity;
+
+        motor.RotateCamera(camRotation);
+    }
+
+    private void HandleWalking()
+    {
+        bool isWalking = Input.GetKey(walk);
+        currentSpeed = isWalking ? walkSpeed : normalSpeed;
+    }
+
+    public void SetInvertLook(bool inv)
+    {
+        invertLook = inv;
+        invertedLook = invertLook ? 1 : -1;
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = this.GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    /// <summary>
+    /// Moves player based on current position, current velocity, and scaled by physics delta time.
+    /// Does nothing if the velocity is zero.
+    /// </summary>
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rigid.MovePosition(rigid.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    /// <summary>
+    /// Rotate the rigidbody of the player in the y plane, and the camera of the player
+    /// in the x plane.
+    /// </summary>
+    private void PerformRotation()
+    {
+        rigid.MoveRotation(rigid.rotation * Quaternion.Euler(rotation));
+        if(cam != null)
+        {
+            cam.transform.Rotate(cameraRotation);
+        }
+    }
+
+    /// <summary>
+    /// Recieves new velocity update from player controller.
+    /// </summary>
+    /// <param name="newVelocity">The velocity the player motor will now have.</param>
+    public void Move(Vector3 newVelocity)
+    {
+        velocity = newVelocity;
+    }
+    
+    /// <summary>
+    /// Set new rotation of the player's rigidbody, or the y plane rotation.
+    /// </summary>
+    /// <param name="newRotation">The new vector 3 of rotation desired.</param>
+    public void RotateY(Vector3 newRotation)
+    {
+        rotation = newRotation;
+    }
+
+    /// <summary>
+    /// Set new rotation of the player's camera, or the x plane rotation
+    /// </summary>
+    /// <param name="newRotation">The new vector 3 of rotation desired.</param>
+    public void RotateCamera(Vector3 newRotation)
+    {
+        cameraRotation = newRotation;
+    }
+
+    /// <summary>
+    /// If there is an active player camera it is set here.
+    /// </summary>
+    /// <param name="cam">Player's camera</param>
+    public void SetCamera(Camera cam)
+    {
+        this.cam = cam;
+    }
+
+}
