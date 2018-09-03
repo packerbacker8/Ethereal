ba656b185f5ef485beb78f714f7c9c5280bc2fc9
packerbacker8 <chandler@gbpackerfans.com>
2018-09-02 18:16:41 -0600

    Starting to make ui element to represent inventory. Need to then hoook to inventory and send to player. Need to hook to economy still

---
+++ Assets/Scripts/Player/InventoryUIScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUIScript : MonoBehaviour
+{
+    public GameObject inventoryAndMoneyPanel;
+    public GameObject overallInventoryUI;
+    public GameObject primaryWepImageObj;
+    public GameObject secondaryWepImageObj;
+    public GameObject knifeImageObj;
+    public GameObject grenadePanelObj;
+    public GameObject objectiveBombImageObj;
+
+    private float uiOpacity = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
